f2abde5c6d2c51866741a9d0f3c67d796bb9c84c
Daira Rodriguez <35277731+dairadriana@users.noreply.github.com>
2023-12-01 09:30:36 -0600

    Imrpoved

--- Scripts/Imagen/ImageAnalysis.py
+++ Scripts/Imagen/ImageAnalysis.py
@@ -20,6 +20,8 @@ screen_height = root.winfo_screenheight()
 # Cliente de visión AI
 client = vision_v1.ImageAnnotatorClient()
 
+image_list = []
+
 # Calcular la posición vertical para centrar la webcam
 vertical_position = (screen_height - (screen_height // 2)) // 2
 
@@ -48,6 +50,24 @@ query_window.attributes('-fullscreen', True)
 # Configurar el fondo negro del widget de texto
 query_text = tk.Text(query_window, wrap=tk.WORD, width=screen_width, height=screen_height, bg='black', fg='green', font=('Arial', 14))
 query_text.pack()
+
+def add_neon_border(image_path):
+    img = Image.open(image_path)
+    
+    # Configurar el color del borde (en este caso, verde neón)
+    border_color = (0, 255, 0)  # R, G, B
+    
+    # Tamaño del borde
+    border_size = 10
+    
+    # Crear una nueva imagen con el borde
+    bordered_img = Image.new("RGB", (img.width + 2 * border_size, img.height + 2 * border_size), border_color)
+    
+    # Pegar la imagen original en el centro de la nueva imagen
+    bordered_img.paste(img, (border_size, border_size))
+    
+    return bordered_img
+
 
 def clear_images(folder_path, max_images=250):
     # Obtener la lista de todas las imágenes en el directorio
@@ -90,8 +110,6 @@ def detect_labels(path):
             "https://cloud.google.com/apis/design/errors".format(response.error.message)
         )
 
-image_list = []
-
 def retrieve_from_google(query, limit):
     folder_path = os.path.join(os.getcwd(), "Images")
 
@@ -121,16 +139,27 @@ def retrieve_from_google(query, limit):
         with open(image_path, 'wb') as f:
             f.write(requests.get(image_url).content)
 
+        # Abrir la imagen con Pillow (PIL)
+        img_pil = Image.open(image_path)
+
+        # Calcular el nuevo ancho manteniendo la relación de aspecto original
+        ratio = 250 / float(img_pil.size[1])
+        new_width = int(float(img_pil.size[0]) * float(ratio))
+        new_height = int(float(img_pil.size[1]) * float(ratio))
+
+        # Redimensionar la imagen
+        img_pil = img_pil.resize((new_width, 250), Image.BILINEAR)
+
         # Calcular posición aleatoria en el lienzo
         img_width = screen_width // 2
         img_height = screen_height
-        img_x = random.randint(0, screen_width - img_width)
-        img_y = random.randint(0, screen_height - img_height)
+        img_x = random.randint(0, screen_width - new_width)
+        img_y = random.randint(0, screen_height - new_height)
 
         # Mostrar la imagen en el lienzo de Tkinter
         img_google = Image.open(image_path)
         img_google = img_google.resize((img_width, img_height))
-        img_google_tk = ImageTk.PhotoImage(img_google)
+        img_google_tk = ImageTk.PhotoImage(img_pil)
 
         canvas.create_image(img_x, img_y, anchor=tk.NW, image=img_google_tk)
         canvas.image = img_google_tk
